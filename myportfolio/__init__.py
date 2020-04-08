e238e34b64554ab2fec5c4a8c52cabbd008d71b9
Michika <mhiratsuka0209@gmail.com>
2020-04-07 17:28:58 -0700

    Added user authetification feature

--- myportfolio/__init__.py
+++ myportfolio/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '82e4e6312839d16f95075c7228437e50'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from myportfolio import routes
